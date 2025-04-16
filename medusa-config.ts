5b4dbd23b772435ca5fd77eed47833f32624e675
sakurayung <jorspacturan13@gmail.com>
2025-04-16 12:58:29 +0800

    changes

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,19 +1,61 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils';
+import { loadEnv, defineConfig, Modules } from '@medusajs/framework/utils';
 
 loadEnv(process.env.NODE_ENV || 'development', process.cwd());
 
 module.exports = defineConfig({
+  admin: {
+    vite: () => {
+      return {
+        server: {
+          allowedHosts: [
+            "473a-143-44-184-88.ngrok-free.app",]
+        }
+      }
+    }
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     http: {
       storeCors: process.env.STORE_CORS || "http://localhost:8000",
-      adminCors: process.env.ADMIN_CORS || "http://localhost:9000",
+      adminCors: process.env.ADMIN_CORS || "https://25c4-143-44-184-88.ngrok-free.app/",
       authCors: process.env.AUTH_CORS || "http://localhost:8000,http://localhost:9000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
   },
   modules: [
+    {
+      resolve: "@medusajs/medusa/payment",
+      key: Modules.PAYMENT,
+      options: {
+        providers: [
+          {
+            id: "paypal",
+            resolve: "medusa-payment-paypal",
+            options: {
+              sandbox: process.env.PAYPAL_SANDBOX,
+              client_id: process.env.PAYPAL_CLIENT_ID,
+              client_secret: process.env.PAYPAL_CLIENT_SECRET,
+              auth_webhook_id: process.env.PAYPAL_AUTH_WEBHOOK_ID,
+            },
+          },
+        ]
+      }
+    },
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/payment-stripe",
+            id: "stripe",
+            options: {
+              apiKey: process.env.STRIPE_API_KEY,
+            },
+          },
+        ],
+      },
+    },
     {
       resolve: "@medusajs/medusa/file",
       options: {
@@ -34,23 +76,6 @@ module.exports = defineConfig({
             },
           },
         ],
-      },
-    },
-    {
-      resolve: "@medusajs/medusa/payment",
-      options: {
-        providers: [
-          {
-            resolve: "./src/modules/my-payment",
-            id: "manual-payment",
-            options: {
-              // provider options...
-              apiKey: process.env.MANUAL_PAYMENT_KEY,
-              merchantId: process.env.MANUAL_PAYMENT_MERCHANT_ID,
-              sandbox: process.env.MANUAL_PAYMENT_SANDBOX === "true",
-            }
-          }
-        ]
       },
     },
     {
