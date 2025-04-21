fbac9c8bd19bbdb31f9c26c374b62f6d21da01cd
sayurinsimp <hello.kenji.dev@gmail.com>
2025-04-21 18:19:32 +0800

    chore: added resend module, lacks email templates, added dependencies (resend, react-email)

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,6 +1,6 @@
-import { loadEnv, defineConfig } from '@medusajs/framework/utils';
+import { loadEnv, defineConfig } from "@medusajs/framework/utils";
 
-loadEnv(process.env.NODE_ENV || 'development', process.cwd());
+loadEnv(process.env.NODE_ENV || "development", process.cwd());
 
 module.exports = defineConfig({
   projectConfig: {
@@ -8,7 +8,8 @@ module.exports = defineConfig({
     http: {
       storeCors: process.env.STORE_CORS || "http://localhost:8000",
       adminCors: process.env.ADMIN_CORS || "http://localhost:9000",
-      authCors: process.env.AUTH_CORS || "http://localhost:8000,http://localhost:9000",
+      authCors:
+        process.env.AUTH_CORS || "http://localhost:8000,http://localhost:9000",
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     },
@@ -30,7 +31,7 @@ module.exports = defineConfig({
               endpoint: process.env.S3_ENDPOINT,
               additional_client_config: {
                 forcePathStyle: true,
-              }
+              },
             },
           },
         ],
@@ -48,13 +49,31 @@ module.exports = defineConfig({
               apiKey: process.env.MANUAL_PAYMENT_KEY,
               merchantId: process.env.MANUAL_PAYMENT_MERCHANT_ID,
               sandbox: process.env.MANUAL_PAYMENT_SANDBOX === "true",
-            }
-          }
-        ]
+            },
+          },
+        ],
       },
     },
+    {
+      resolve: "@medusajs/medusa/notification",
+      options: {
+        providers: [
+          {
+            resolve: "./src/modules/resend",
+            id: "resend",
+            options: {
+              channels: ["email"],
+              api_key: process.env.RESEND_API_KEY,
+              from: process.env.RESEND_FROM_EMAIL,
+            },
+          },
+        ],
+      },
+    },
+
     {
       resolve: "./src/modules/receipt",
     },
   ],
 });
+
