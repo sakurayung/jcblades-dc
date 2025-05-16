15a4189b8b9a2fedf66e4efc955c0caf84645e5a
sakurayung <jorspacturan13@gmail.com>
2025-05-16 13:54:16 +0800

    revalidating cache for the reflecting changes

---
+++ src/subscribers/product-update.ts
@@ -0,0 +1,12 @@
+import type { SubscriberArgs, SubscriberConfig } from "@medusajs/framework";
+
+export default async function productUpdateHandler({
+    event: { data },
+    container,
+}: SubscriberArgs<{ id: string }>) {
+    await fetch(`${process.env.STOREFRONT_URL}/api/revalidate?tags=products`)
+}
+
+export const config: SubscriberConfig = {
+    event: "product.updated"
+}
