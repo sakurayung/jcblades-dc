10cfc6e90027ffec9e4653860b84f60f96b92b0b
sakurayung <jorspacturan13@gmail.com>
2025-05-17 18:02:51 +0800

    email notification error

--- src/subscribers/emailNotification.ts
+++ src/subscribers/emailNotification.ts
@@ -1,14 +1,16 @@
 import { sendCustomerVerificationWorkflow } from "../workflows/send-email-confirmation";
+import type { SubscriberArgs, SubscriberConfig } from "@medusajs/framework";
+
 
 export default async function userVerificationHandler({
   event: { data },
   container,
-}) {
+}: SubscriberArgs<{ id: string }>) {
   await sendCustomerVerificationWorkflow(container).run({
     input: { customerId: data.id },
   });
 }
 
-export const config = {
+export const config: SubscriberConfig = {
   event: "customer.created",
 };
